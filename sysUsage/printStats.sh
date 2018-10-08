e35ff88782c5c07fa37be63618dd6392b785afa0
Ben <the12thbenyoung@gmail.com>
2018-10-07 20:41:50 -0400

    Added basic clock and system usage backend

---
+++ sysUsage/printStats.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#script to print system's current CPU usage, memory usage, and disk usage
+
+#CPU usage
+#values from 0.3 seconds earlier
+old_stats=$(cat /proc/stat | head -1)
+#sum of active CPU cycles (system, nice, and user)
+let old_work=$(echo $old_stats | awk '{print $2+$3+$4}')
+#sum of all CPU cycles
+let old_total=$old_work+$(echo $old_stats | awk '{print $5+$6+$7+$8+$9+$10}')
+
+sleep 0.3
+
+#current values
+new_stats=$(cat /proc/stat | head -1)
+#sum of active CPU cycles (system, nice, and user)
+let new_work=$(echo $new_stats | awk '{print $2+$3+$4}')
+#sum of all CPU cycles
+let new_total=$new_work+$(echo $new_stats | awk '{print $5+$6+$7+$8+$9+$10}')
+
+#ratio of differences between working cycles and total cycles give %CPU usage over this interval
+echo CPU Usage: $(bc <<< "scale=1; 100 * ($new_work - $old_work) / ($new_total - $old_total)")%
+
+#Memory usage
+free -m | grep "Mem:" | awk '{printf "Memory Usage: %.1f%% (%dM of %dM)\n", $3/$2*100, $3, $2}'
+
+#Disk usage
+df -h | grep "/home" | awk '{printf "Disk Usage: %s (%s of %s)\n", $5, $3, $4}'
+
+
---
+++ sysUsage/sysUsage.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	while(true){
+		system("./printStats.sh");
+		usleep(400000);
+	}
+	return 0;
+}
